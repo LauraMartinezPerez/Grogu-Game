3b8d26b886236dfaf5bf118ba21a0f7feadfc4d5
LauM25 <laurammwork93@gmail.com>
2025-04-01 11:21:38 +0200

    Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import Board from "./Board";
 import Dice from "./Dice";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
+import Instructions from "./Instructions";
 
 function App() {
   const [groguPosition, setGroguPosition] = useState(0);
@@ -12,17 +15,21 @@ function App() {
   const [diceValue, setDiceValue] = useState(null);
   const [gameStatus, setGameStatus] = useState("En curso");
 
-const randomNumber = (max) => {
-  return Math.floor(Math.random() * max) + 1;
-};
+  const randomNumber = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+  };
 
-useEffect(() => {
-if (groguPosition === 6) {
-    setGameStatus("¡¡¡Has perdido, Grogu se ha comido el cargamento!!!");
-  }  else if (goodsCookies.length === 0 && goodsEggs.length === 0 && goodsFrog.length === 0) {
-    setGameStatus("¡¡¡Ganaste, Mando completa la misión!!!");
-  }  
-/*  if (goodsCookies.length === 0 && goodsEggs.length === 0 && goodsFrog.length === 0) {
+  useEffect(() => {
+    if (groguPosition === 6) {
+      setGameStatus("¡¡¡Has perdido, Grogu se ha comido el cargamento!!!");
+    } else if (
+      goodsCookies.length === 0 &&
+      goodsEggs.length === 0 &&
+      goodsFrog.length === 0
+    ) {
+      setGameStatus("¡¡¡Ganaste, Mando completa la misión!!!");
+    }
+    /*  if (goodsCookies.length === 0 && goodsEggs.length === 0 && goodsFrog.length === 0) {
     setGameStatus("¡¡¡Ganaste, Mando completa la misión!!!");
 
 else if (goodsCookies.length === 0) {
@@ -34,57 +41,55 @@ else if (goodsCookies.length === 0) {
      else if (goodsFrog.length === 0) {
     setGameStatus("Te has comido todas las ranas, glotona!!!")
   }   */
+  }, [groguPosition, goodsCookies, goodsEggs, goodsFrog]);
 
+  const rollDice = () => {
+    const getRandomNumber = randomNumber(4);
+    setDiceValue(getRandomNumber);
 
-}, [groguPosition, goodsCookies, goodsEggs, goodsFrog ]);
-
-
-
-const rollDice = () => {
-  
-  const getRandomNumber = randomNumber(4);
-  setDiceValue(getRandomNumber);
-
-  if (getRandomNumber === 4) {
+    if (getRandomNumber === 4) {
       setGroguPosition(groguPosition + 1);
       setGameStatus("¡¡¡Cuidado, Grogu avanza una casilla!!!");
-  } else if (getRandomNumber === 1) {
+    } else if (getRandomNumber === 1) {
       setGoodsCookies(goodsCookies.slice(1));
       setGameStatus("Has ayudado a Mando a recoger una galleta");
-  } else if (getRandomNumber === 2) {
-    setGoodsEggs(goodsEggs.slice(1));
-    setGameStatus("Has ayudado a Mando a recoger un huevo");
-  } else {
-    setGoodsFrog(goodsFrog.slice(1));
-    setGameStatus("Has ayudado a Mando a recoger una rana");
-  } 
-
-} 
-
+    } else if (getRandomNumber === 2) {
+      setGoodsEggs(goodsEggs.slice(1));
+      setGameStatus("Has ayudado a Mando a recoger un huevo");
+    } else {
+      setGoodsFrog(goodsFrog.slice(1));
+      setGameStatus("Has ayudado a Mando a recoger una rana");
+    }
+  };
 
   return (
     <div className="page">
       <Header />
       <main className="main">
-        <Board onGroguPosition={groguPosition}/>
-        <section className="diceSection"> 
-          <Dice className="dice" onRollDice={rollDice}/>
-         <p>{diceValue}</p>
-          <div className="game-status">{gameStatus}</div>
-        </section>
-        <section className="goods-container">
-          <div className="goods-item">{goodsCookies}</div>
-        </section>
-        <section className="goods-container">
-          <div className="goods-item">{goodsEggs}</div>
-        </section>
-        <section className="goods-container">
-          <div className="goods-item">{goodsFrog}</div>
-        </section>
-        <section>
-          <button className="restart-button">Reiniciar Juego</button>
-        </section>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Instructions" element={<Instructions />} />
+          <Board onGroguPosition={groguPosition} />
+          <section className="diceSection">
+            <Dice className="dice" onRollDice={rollDice} />
+            <p>{diceValue}</p>
+            <div className="game-status">{gameStatus}</div>
+          </section>
+          <section className="goods-container">
+            <div className="goods-item">{goodsCookies}</div>
+          </section>
+          <section className="goods-container">
+            <div className="goods-item">{goodsEggs}</div>
+          </section>
+          <section className="goods-container">
+            <div className="goods-item">{goodsFrog}</div>
+          </section>
+          <section>
+            <button className="restart-button">Reiniciar Juego</button>
+          </section>
+        </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+//<NavLink to="/"> <NavLink />;
+//importar link
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <nav>
+        <ul>
+          <li class="footer__menu-item">
+            <a class="footer__menu-link" href="#/">
+              A jugar
+            </a>
+          </li>
+          <li class="footer__menu-item">
+            <a class="footer__menu-link active" href="#/instructions">
+              ¿Cómo se juega?
+            </a>
+          </li>
+          <li class="footer__menu-item">
+            <a class="footer__menu-link" href="#/options">
+              Más opciones
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <small class="footer__copy">© Adalab</small>
+    </footer>
+  );
+};
+export default Footer;
